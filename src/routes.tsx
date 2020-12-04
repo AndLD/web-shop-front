4b87ef35182b6ccbb9a4c2c7108ec933fd670726
Timur <timur.sholokh@gmail.com>
2020-12-04 23:19:40 +0300

    Финальный вариант файловой структуры.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import {Switch, Route, Redirect} from "react-router-dom"
 
+import {Slider} from "./components/Slider/Slider"
+
 export const useRoutes = () => {
     return (
         <Switch>
 
             <Route path="/" exact>
-                <div className="container">
-                    
-                </div>
+                <Slider />
             </Route>
 
             <Redirect to="/" />
