a8860c12f8735dc2b589c54f2f8f15978799b910
Timur <timur.sholokh@gmail.com>
2020-12-05 00:29:32 +0300

    Конечная-конечная настройка.

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -5,12 +5,11 @@ import './slider.scss'
 export class Slider extends React.Component{
     render() {
         return (
-            <div className="slider">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            dsdsdsdsd <br/>
-                            sdfdsfdsfsdfds
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="slider">
+                            
                         </div>
                     </div>
                 </div>
