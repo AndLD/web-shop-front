4eea752a65b512536e44d571ecde69422194ff79
Timur <timur.sholokh@gmail.com>
2021-01-07 14:52:40 +0300

    Начал делать отображение списка товаров.

--- src/components/ItemsWrapper/ItemsWrapper.jsx
+++ src/components/ItemsWrapper/ItemsWrapper.jsx
@@ -1,32 +1,34 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import {ItemCard} from "../ItemCard/ItemCard"
 import './itemsWrapper.scss'
 
 export const ItemsWrapper = (props) => {
-    let itemData = [], typeRender = props.typeRender || "all"
+    let itemData, 
+        typeRender = props.typeRender || "all"
+    const [data, updateData] = useState(0);
+
     async function getAllItems () {
         try {
             const response = await fetch('http://localhost:8080/products', {
                 method: 'GET'
             });
             return await response.json();
-        } catch (error) {
-            console.error('Ошибка:', error);
-        }
+        } catch (error) {}
     }
 
-    useEffect(async () => {
-      if(typeRender = "all-items") itemData.push(await getAllItems())     
-      console.log(itemCard) 
-    })
+     useEffect(() => {
+        (async function () {
+            if(typeRender = "all-items") itemData = await getAllItems() || []
+            let masCards = itemData.map((data) => 
+                <ItemCard key={data._id} itemID={data._id} imgBlob={data.images} title={data.title} price={data.price}/>
+            )
+            updateData(masCards) 
+         }());
+     }, [])
 
     return(
-        <div className="item_wr d-flex justify-content-between b-white">
-            {/* <ItemCard itemID={1} imgSrc={1} title={"Видеокарта Gigabyte GeForce GTX 1660 SUPER OC 6144MB (GV-N166SO)"} price={"10 400"}/>
-            <ItemCard itemID={2} imgSrc={2} title={"Видеокарта Asus GeForce RTX 2070 Turbo 8192MB (TURBO-RTX2070-8G"} price={"10 400"}/>
-            <ItemCard itemID={3} imgSrc={3} title={"Видеокарта MSI GeForce GTX 1660 SUPER VENTUS XS OC 6144MB (GTX  "} price={"10 400"}/>
-            <ItemCard itemID={4} imgSrc={1} title={"Видеокарта Gigabyte GeForce GTX 1650 D6 WindForce OC 4096MB (GV"} price={"10 400"}/>
-            <ItemCard itemID={5} imgSrc={2} title={"Видеокарта Palit GeForce GTX 1660 SUPER StormX 6144MB (NE6166S0"} price={"10 400"}/> */}
+        <div className="item_wr b-white">
+            {data}
         </div>
     )
 }
---
+++ src/components/SlavicAdminPanel/adminComponents/DisplayGoods/DisplayGoods.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './displayGoods.scss'
+import {Input} from '../../../Input/Input'
+
+export class DisplayGoods extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+    }
+
+    getRow() {
+        return(
+            <></>
+        )
+    }
+
+    render() {
+        return (
+            <div className="display-goods">
+               
+                <div className="goods-table_wr">
+
+                    <div className="goods-table-title d-flex justify-content-between">
+                        <h2>Список товаров</h2>
+                        <div className="btn-goods-wr">
+                            <button>1</button>
+                            <button>2</button>
+                            <button>3</button>
+                        </div>
+                    </div>
+
+                    <div className="goods-table">
+                        <table>
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td>Изображение</td>
+                                <td>Название товара</td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+                <div className="goods-filter_wr">
+                    <div className="goods-filter-title d-flex justify-content-between">
+                        <label htmlFor="">3132313123</label>
+                        <input type="text"/>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
